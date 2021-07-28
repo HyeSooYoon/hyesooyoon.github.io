741660be244ac14675666312fe30e4fd047d1761
HyeSoo Yoon <78719881+HyeSooYoon@users.noreply.github.com>
2021-07-28 21:04:39 +0900

    vue store 연동

---
+++ vd/src/main/resources/static/vue/surfVideoStore.js
@@ -0,0 +1,11 @@
+const surfVideoStore = {
+    state: {
+        videoNo: '8nv2wE1nu-E',
+        count: 0
+  },
+  mutations: { 
+    increment (state) { 
+      state.count++ 
+    } 
+  } 
+}
--- vd/src/main/resources/static/vue/surfVideoVue.js
+++ vd/src/main/resources/static/vue/surfVideoVue.js
@@ -1,19 +1,18 @@
-Vue.component('surfvideo-view', {  
-  methods: {  
-  },
-  template: 
-  `<ul style="z-index: 1;">
-   <li><iframe width="560" id="vd" height="315" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
-  
-  
-</ul>`,
-props: {
-  vurl  : String 
-},
+Vue.component('surfvideo-view', {    
+template: 
+`
+  <ul style="z-index: 1;">   
+    <li><iframe width="560" id="youtubevd" height="315" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>  
+  </ul>`,  
 mounted() {  
-  document.getElementById('vd').setAttribute("src", "https://www.youtube.com/embed/" + this.vurl);
+  document.getElementById('youtubevd').setAttribute("src", this.youtubeUrl);
+}, 
+data() { 
+  return { 
+    youtubeUrl: 'https://www.youtube.com/embed/' + this.$store.state.videoNo
+  } 
 }
-
+ 
 });
 
 
