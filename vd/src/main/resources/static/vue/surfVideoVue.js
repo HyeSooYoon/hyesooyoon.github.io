d1f4d048695c4a53d48d4f49a6fd3ec0fa3c354b
HyeSoo Yoon <78719881+HyeSooYoon@users.noreply.github.com>
2021-08-01 22:44:07 +0900

    css 수정

--- vd/src/main/resources/static/vue/surfVideoVue.js
+++ vd/src/main/resources/static/vue/surfVideoVue.js
@@ -1,7 +1,11 @@
 Vue.component('surfvideo-view', {    
   template: 
 `
-<ul id="youtubeVideoList" style="z-index: 1;"></ul>
+<ul id="youtubeVideoList" style="z-index: 1;">
+<li><iframe width="560" height="315" src="https://www.youtube.com/embed/x9VYKrtziSg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
+<li><iframe width="560" height="315" src="https://www.youtube.com/embed/x9VYKrtziSg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
+</ul>
+
 `  
 });
 
