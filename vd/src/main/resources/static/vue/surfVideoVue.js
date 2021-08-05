aafa2f83c65a58291e39b4525a93bd6154c66e77
HyeSoo Yoon <78719881+HyeSooYoon@users.noreply.github.com>
2021-08-05 22:50:13 +0900

    twitch, youtube 동영상 배치 정리

--- vd/src/main/resources/static/vue/surfVideoVue.js
+++ vd/src/main/resources/static/vue/surfVideoVue.js
@@ -1,6 +1,13 @@
 Vue.component('surfvideo-view', {    
   template: 
-`<ul id="videoList" style="z-index: 1;background-color: #5c5cb7;"></ul>`  
+`
+<ul id="videoList" style="z-index: 1;background-color: #5c5cb7;">
+<img id="youtube_logo" src="/images/YouTube-Logo.png" style="object-fit: cover; width: 100px; margin: 15px; display: none;"/><br>
+<li id="youtubeVideoList"></li><br>
+<img id="twitch_logo" src="/images/Twitch-Logo.png" style="object-fit: cover; width: 100px; margin: 15px; display: none;"/><br>
+<li id="twitchVideoList"></li><br>
+</ul>
+`  
 }); 
 
 
