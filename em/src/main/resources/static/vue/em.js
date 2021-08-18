6c0b9df7aea4a9e11171230033533ac2461ce925
HyeSoo Yoon <78719881+HyeSooYoon@users.noreply.github.com>
2021-08-18 21:46:06 +0900

    저장기능 구현..

--- em/src/main/resources/static/vue/em.js
+++ em/src/main/resources/static/vue/em.js
@@ -161,7 +161,7 @@ Vue.component('emmain-view', {
        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-paperclip">
         <path d="M21.44 11.05l-9.19 9.19a6 6 0 01-8.49-8.49l9.19-9.19a4 4 0 015.66 5.66l-9.2 9.19a2 2 0 01-2.83-2.83l8.49-8.48" /></svg>
       </div>
-      <div class="send" @click="add">
+      <div class="send" @click="list">
        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-send">
         <path d="M22 2L11 13M22 2l-7 20-4-9-9-4 20-7z" /></svg>
       </div>
@@ -272,9 +272,9 @@ data() {
   return { now: moment(new Date()).format('DD MMM, YYYY') }
 },
 methods:{
-  add: function() { 
-    
-    fetch('http://localhost:5013/add', {
+  add: function() {
+    return new Promise((resolve) => { 
+      fetch('http://localhost:5013/add', {
       method: 'post', 
       headers: {
       'Content-Type': 'application/json'
@@ -284,15 +284,26 @@ methods:{
         title: document.getElementsByName("title")[0].value
         })
       })
-      .then(res => res.json())
-      .then(function(data) {
-        if(data.code==='0000')
+      .then(res => resolve(res.json())) 
+    })    
+  },
+  list: function() { 
+    this.add().then(function(data) {
+        if(!data.success)
+        { 
+          alert(data.message); 
+          return;
+        }        
+        else
         {
-          
+          alert('조회');
+
         }
-        
-      })
+    })
+   
   }
+
+  
 } 
 }); 
 
